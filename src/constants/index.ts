0c6049619c6d4626bc681ecc6844d82100f4398c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-10 01:31:16 +0200

    chore(documents): add barrel export for all editor constants

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Barrel export for all editor constants.
+ */
+export type { MenuItem, MenuItemAction, MenuItemDivider } from './types';
+export { GOOGLE_FONTS, FONT_SIZES, loadGoogleFont } from './google-fonts';
+export type { GoogleFont } from './google-fonts';
+export { FILE_MENU_ITEMS } from './file-menu';
+export { EDIT_MENU_ITEMS } from './edit-menu';
+export { VIEW_MENU_ITEMS } from './view-menu';
+export { INSERT_MENU_ITEMS } from './insert-menu';
+export { FORMAT_MENU_ITEMS } from './format-menu';
+export { TOOLS_MENU_ITEMS } from './tools-menu';
+export { EXTENSIONS_MENU_ITEMS } from './extensions-menu';
+export { HELP_MENU_ITEMS } from './help-menu';
