957a903b3a52a277c900e0cb6b8ba27e1f8d4fc0
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-10 14:06:18 +0200

    chore(documents): add barrel export for documents page components

---
+++ src/components/pages/documents/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Barrel export for documents page components.
+ */
+export { DocumentCard, STATUS_LABELS } from './DocumentCard';
+export { DocumentsFilters, SORT_OPTIONS } from './DocumentsFilters';
+export type { StatusFilter, SortOption } from './DocumentsFilters';
