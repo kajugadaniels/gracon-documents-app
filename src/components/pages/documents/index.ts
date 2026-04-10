e2d0ccc0e09e806f3c54e434bab33301dfb83e03
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-10 14:29:56 +0200

    chore(ui): export DeleteDocumentDialog from documents barrel

--- src/components/pages/documents/index.ts
+++ src/components/pages/documents/index.ts
@@ -3,4 +3,5 @@
  */
 export { DocumentCard, STATUS_LABELS } from './DocumentCard';
 export { DocumentsFilters, SORT_OPTIONS } from './DocumentsFilters';
+export { DeleteDocumentDialog } from './DeleteDocumentDialog';
 export type { StatusFilter, SortOption } from './DocumentsFilters';
