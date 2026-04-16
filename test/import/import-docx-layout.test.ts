0b31c0c5ad3c975b50a71d87a3b1117d6174035b
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-16 22:44:58 +0200

    test: cover DOCX tab stop import

--- test/import/import-docx-layout.test.ts
+++ test/import/import-docx-layout.test.ts
@@ -4,6 +4,8 @@ import { describe, it } from 'node:test';
 import {
     collectImportedParagraphLayouts,
     createImportedParagraphLayout,
+    extractParagraphTabStopsFromDocumentXml,
+    mergeParagraphTabStopsIntoLayouts,
     twipToPx,
 } from '../../src/lib/import-docx-layout.ts';
 
@@ -95,6 +97,53 @@ describe('DOCX import layout conversion', () => {
 
         assert.deepEqual(layouts, [
             { leftIndent: 48, firstLineIndent: 0, tabStops: [] },
+            { leftIndent: 0, firstLineIndent: -24, tabStops: [] },
+        ]);
+    });
+
+    it('extracts exact paragraph tab stops from DOCX document XML', () => {
+        const tabStops = extractParagraphTabStopsFromDocumentXml(`
+            <w:document>
+                <w:body>
+                    <w:p>
+                        <w:pPr>
+                            <w:tabs>
+                                <w:tab w:val="left" w:pos="720"/>
+                                <w:tab w:val="left" w:pos="1440"/>
+                            </w:tabs>
+                        </w:pPr>
+                        <w:r><w:t>First</w:t></w:r>
+                    </w:p>
+                    <w:p>
+                        <w:pPr>
+                            <w:tabs>
+                                <w:tab w:val="center" w:pos="2160"/>
+                                <w:tab w:val="left" w:pos="2880"/>
+                            </w:tabs>
+                        </w:pPr>
+                    </w:p>
+                    <w:p><w:r><w:t>No tabs</w:t></w:r></w:p>
+                </w:body>
+            </w:document>
+        `);
+
+        assert.deepEqual(tabStops, [[48, 96], [192], []]);
+    });
+
+    it('merges raw DOCX tab stops into Mammoth paragraph layouts by paragraph order', () => {
+        const layouts = mergeParagraphTabStopsIntoLayouts(
+            [
+                { leftIndent: 48, firstLineIndent: 0, tabStops: [] },
+                { leftIndent: 0, firstLineIndent: -24, tabStops: [] },
+            ],
+            [
+                [48, 96],
+                [],
+            ],
+        );
+
+        assert.deepEqual(layouts, [
+            { leftIndent: 48, firstLineIndent: 0, tabStops: [48, 96] },
             { leftIndent: 0, firstLineIndent: -24, tabStops: [] },
         ]);
     });
